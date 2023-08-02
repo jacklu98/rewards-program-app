6d42f965dfe04eb5bef81b50392bb706bcacf525
Jack Lu <cnlujc@gmail.com>
2023-08-02 15:51:14 -0400

    second commit

--- src/__tests__/utils/useRequestAllTransactions.test.js
+++ src/__tests__/utils/useRequestAllTransactions.test.js
@@ -1,13 +1,33 @@
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor, act } from "@testing-library/react";
 import useRequestAllTransactions from "../../utils/hooks/useRequestAllTransactions";
-import { getAllTransactions } from "../../api/mockApi";
+import { transactions as mockData, getAllTransactions } from "../../api/mockApi";
+
+function requestFail() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => reject("request failed"), 1000);     
+    })
+}
 
 describe('useRequestAllTransactions hook', () => {
-    it('should render correct states', () => {
-        const { result } = renderHook(() => useRequestAllTransactions(getAllTransactions))
-        const [transactionsGroupByCustomer, customers, timePeriod] = result.current;
-        expect(transactionsGroupByCustomer).toBeInstanceOf(Object);        
-        expect(Array.isArray(customers)).toBe(true);
-        expect(Array.isArray(timePeriod)).toBe(true);
+    it('should render loading status and correct states', async() => {
+        const { result } = renderHook(() => useRequestAllTransactions(getAllTransactions));
+        const [transactions, customers, timePeriod, isLoading, errorMsg] = result.current;
+        expect(transactions).toEqual([]);        
+        expect(customers).toEqual([]); 
+        expect(timePeriod).toEqual([]);
+        expect(errorMsg).toBeNull();
+        expect(isLoading).toBe(true);
+
+        await waitFor(() =>expect(result.current[0]).toEqual(mockData), {timeout:1500});
+        
+        expect(result.current[1]).toEqual(['T8R11E', 'A2BS4F', 'CB1S11', 'T8JF91', 'T367KB', 'CBUT4A']); 
+        expect(result.current[2]).toEqual(['April 2023', 'March 2023', 'February 2023']);
+        expect(result.current[3]).toBe(false);
+        expect(result.current[4]).toBeNull();
     });
+    it("should show error message after request fail", async() => {
+        const { result } = renderHook(() => useRequestAllTransactions(requestFail));
+        await waitFor(() => expect(result.current[3]).toBe(false), {timeout:1500});
+        expect(result.current[4]).toBeTruthy();
+    })
 })
